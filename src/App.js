fecd2c691f2c1102f8f26a40c165c4cf0e9bf77d
pooh7159 <pooh6043@gmail.com>
2022-03-20 16:38:59 +0900

    gh pages test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import Movie from "./components/Movie";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+      <Route path="/detail/:id" element={<Detail />} />
+    </Routes>
   )
 }
 export default App;

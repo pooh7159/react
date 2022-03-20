0223dc9efa9de54bc5e069600ce2e18fb3e925b7
pooh7159 <pooh6043@gmail.com>
2022-03-20 17:23:06 +0900

    react router v6 + gh-pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
-import {Route, Routes } from "react-router-dom";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Routes>
-      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-      <Route path="/detail/:id" element={<Detail />} />
-    </Routes>
+    // BrowserRouter as Router 로 감싸 주어야 동작함
+    <Router>
+      <Routes>
+        {/* process.env.PUBLIC_URL를 붙여줘야 gh-pages 에서 인식함 로컬로 돌릴 떄에는 react/ 가 root가 됨 */}
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
+    </Router>
   )
 }
 export default App;

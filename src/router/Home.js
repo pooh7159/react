ac59dfc5029bafaf5aba39596eb685afd55fc0fd
pooh7159 <pooh6043@gmail.com>
2022-03-29 05:27:20 +0900

    ing...2

--- src/router/Home.js
+++ src/router/Home.js
@@ -1,5 +1,3 @@
-import { useEffect, useState } from "react";
-import Movie from "../components/Movie";
 import navList from "../atom/NavList";
 import { Link } from "react-router-dom";
 import Slide from "../components/Slide";
@@ -13,8 +11,8 @@ function Home() {
           <div className={styles.slide_box}>
             <h3 className={styles.title}>
               <Link to={`/page/${slide.path}/1`}>
-                <i></i>
-                <span>{slide.title}</span>
+                <i class="fas fa-external-link-alt"></i>
+                <span>{slide.title} Movie</span>
               </Link>
             </h3>
             <Slide ytsApi={`https://yts.mx/api/v2/list_movies.json?limit=10&${slide.path}&sort_by=year`} />
